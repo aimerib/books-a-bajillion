6d14426d3e9a8bb4faf8794ae853d80ed94d6262
Aimeri Baddouh <aimeri@baddouh.me>
2019-05-02 13:21:21 -0400

    Update environment variable constant

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const primaryColor = "rgba(102,37,103,0.75)";
-export const googleBooksAPIKey = process.env.GOOGLE_BOOKS_API_KEY;
+export const googleBooksAPIKey = process.env.REACT_APP_GOOGLE_BOOKS_API_KEY;

88fd659bee87e6332560eaa74693ba5d7343fa8f
Aimeri Baddouh <aimeri@baddouh.me>
2019-05-03 09:08:12 -0400

    Fix alignment

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,6 +9,7 @@ const Wrapper = styled.div`
   top: 0;
   z-index: 3;
   margin-bottom: 50px;
+  min-width: 300px;
 `;
 
 const NavBar = styled.div`
@@ -23,7 +24,7 @@ const NavBar = styled.div`
   grid-template-areas:
     "Sort"
     "Search";
-
+  min-width: 300px;
   @media (min-width: 1190px) {
     display: flex;
     align-items: center;
@@ -31,14 +32,16 @@ const NavBar = styled.div`
 `;
 
 const NavbarOpenCloseButton = styled.button`
+  display: none;
   @media (max-width: 1190px) {
+    display: block;
     height: 50px;
     width: 50px;
     background-color: purple;
     border-radius: 0 0 50% 50%;
     position: absolute;
     left: 50%;
-    transform: translateX(calc(-50% - 10px));
+    transform: translateX(calc(-50% - 9px));
     border: none;
     cursor: pointer;
   }
--- src/components/Navbar/SearchBar/SearchBar.js
+++ src/components/Navbar/SearchBar/SearchBar.js
@@ -7,14 +7,17 @@ import { primaryColor } from "../../../constants";
 
 const useStyles = makeStyles({
   search: {
+    margin: "auto",
     borderRadius: 4,
     position: "relative",
     backgroundColor: "white",
     border: "1px solid #ced4da",
     width: "80vw",
     padding: 10,
+    minWidth: 260,
     "@media (min-width: 550px)": {
-      width: "30vw"
+      width: "30vw",
+      marginTop: 20
     }
   },
   searchInput: {
@@ -27,14 +30,16 @@ const ClearButton = styled.button`
   color: ${primaryColor};
   font-size: 2em;
   font-weight: bold;
-  margin: 20px;
-  padding: 0 1em;
-  border: 5px solid ${primaryColor};
+  margin: 10px auto;
+  padding: 0px -3.5em;
+  border: 5px solid rgba(102, 37, 103, 0.75);
   border-radius: 3px;
   height: 58px;
   cursor: pointer;
+  -webkit-transition: linear 100ms;
   transition: linear 100ms;
   width: 85vw;
+  transform: translateX(-10px);
   &:hover {
     background: ${primaryColor};
     border: 5px solid rgba(102, 37, 103, 0);
@@ -49,11 +54,20 @@ const ClearButton = styled.button`
   }
   @media (min-width: 550px) {
     width: 176px;
+    margin: 20px;
+  }
+  @media (max-width: 550px) {
+    min-width: 260px;
   }
 `;
 const Wraper = styled.div`
   grid-area: Search;
-
+  display: block;
+  min-width: 300px;
+  @media (max-width: 550px) {
+    display: flex;
+    flex-wrap: wrap;
+  }
   @media (min-width: 1190px) {
     position: absolute;
     right: 30px;
@@ -91,7 +105,7 @@ export default function SearchBar() {
     <Wraper>
       <TextField
         id="standard-full-width"
-        style={{ margin: 20 }}
+        style={{ transform: "translateX(-10px)" }}
         placeholder="Search Book Titles"
         fullWidth
         margin="normal"

fd6c30b81a33bf38864a099800ce0cc509b82087
Aimeri Baddouh <aimeri@baddouh.me>
2019-05-03 16:13:43 -0400

    Fix loader positioning on smaller screens

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,8 +4,8 @@ import spinner from "../assets/spinner.svg";
 import { primaryColor } from "../constants";
 
 const spin = keyframes`
-  from{transform: rotate(-90deg) scale(0.5);}
-  to{transform: rotate(270deg) scale(0.5);}
+  from{transform: rotate(-90deg) scale(0.3);}
+  to{transform: rotate(270deg) scale(0.3);}
 `;
 
 // Here we create a component that will rotate everything we pass in over two seconds
@@ -18,12 +18,20 @@ const Spinner = styled.div`
 `;
 
 const Loader = styled.div`
+  width: 60vw;
+  height: 60vh;
   display: grid;
   align-items: center;
   justify-items: center;
   background-color: #fafafa;
   border-radius: 10px;
   box-shadow: 0px 10px 25px -5px rgb(0, 0, 0);
+  @media (max-width: 550px) {
+    width: 90vw;
+    height: 90vh;
+    display: flex;
+    flex-direction: column;
+  }
 `;
 const Wraper = styled.div`
   width: 100vw;
@@ -32,7 +40,6 @@ const Wraper = styled.div`
   align-items: center;
   justify-items: center;
   color: ${primaryColor};
-  transition: 500ms ease-out;
 `;
 
 export default function Loading() {
